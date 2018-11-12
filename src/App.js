0b213c5cd1358fd60dca1e53c8852981dc0239df
Syifa Aulia <16053440@BTPN>
2018-11-12 13:38:52 +0700

    test

---
+++ src/App.js
@@ -0,0 +1,21 @@
+//@flow
+
+import * as React from 'react';
+import {View, Text} from 'react-native';
+
+type GreetingProps = {
+  name: string,
+};
+function Greeting(props: GreetingProps) {
+  return <Text>{props.name}</Text>;
+}
+
+function App() {
+  return (
+    <View>
+      <Greeting name={'Syifa'} />
+    </View>
+  );
+}
+
+export default App;
